91a133f11bdd8cd9e8fbb93e2eb137ca90d275b0
Samuele Tosatto <samueletosatto@Samueles-MacBook-Pro.local>
2021-10-02 15:19:35 -0600

    expose Close ClosedLSTD

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,4 +1,7 @@
+from algorithms.closed.lstd_closed import ClosedLSTD
 from algorithms.closed.lstd_gamma_closed import ClosedLSTDGamma
 from algorithms.closed.semi_gradient_closed import ClosedSemiGradient
 
 from algorithms.lstd_gamma import LSTDGamma
+from algorithms.semi_gradient import SemiGradient
+from algorithms.gamma_rc import GammaRC
