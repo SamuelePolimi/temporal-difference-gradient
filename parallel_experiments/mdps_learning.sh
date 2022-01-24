3914546556e3b43aa36b9d3f6d2095938042f4e8
SamuelePolimi <samuele.tosatto@gmail.com>
2022-01-24 14:11:53 -0700

    experiments for franka

--- parallel_experiments/mdps_learning.sh
+++ parallel_experiments/mdps_learning.sh
@@ -4,9 +4,9 @@ cd ..
 
 for j in $(seq 0 4)
 do
-    for i in $(seq 0 9)
+    for i in $(seq 0 19)
     do
-        python3 -m  experiments.mdps_learning $(($j*10 + $i)) &
+        python3 -m  experiments.mdps_learning $(($j*20 + $i)) &
     done
     wait
 done
